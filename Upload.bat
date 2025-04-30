2737b3467c8bfa79f610b56e8caa4b4fcf00d2ee
Sublustrum007 <Sublustrum007@yandex.ru>
2025-04-30 19:38:40 +0300

    'Create file Upload.bat' SKANER 30/04/2025 19:38:40

---
+++ Upload.bat
@@ -0,0 +1,7 @@
+git add ./ 
+timeout /t 15 /nobreak 
+set /p userName="Please enter your Name: " 
+set /p commit="Please enter the commit: " 
+git commit -m git commit -m "'' name=sublustrum007 ComputerName=SKANER 30/04/2025 19:38:40" 
+timeout /t 15 /nobreak 
+git push https://github.com/Sublustrum007ru/ForTest.git 
